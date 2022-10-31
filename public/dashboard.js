5ddde4bbb9bc21ecac16879871a2edeb96a2bdec
gbull19 <gbull19@yahoo.com>
2022-10-31 15:49:50 -0600

    css for cards, hardcoded card option for proof

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,28 +1,65 @@
 const allApptsDiv = document.querySelector('#all_appts');
+const userWelcome = document.querySelector('#user_welcome');
 
 const makeAppointmentCard = (appt) => {
-    const { app_date, interior, appt_price, street_address, city, state} = appt;
-    if (interior == true) { interior = "Yes" } else { interior = "No" }
-    return `<div class="appt-card">
-            <h2>${street_address}, ${city}, ${state}</h2>
-            <h3>${app_date}</h3>
-            <P>Price: ${appt_price}</P>
-            <p>Was Interior Treated?: ${interior} damage</p>
+    const { appt_date, interior, appt_price, street_address, city, state} = appt;
+    let interiorService = ""
+    if (interior == true) { interiorService = "Yes" } else { interiorService = "No" }
+    return `
+        <div class="appt-card-border">
+            <div class="appt-card">
+                <h2>${appt_date}</h2>
+                <h3>${street_address}, ${city}, ${state}</h3>
+                <P>Price: ${appt_price}</P>
+                <p>Was Interior Treated?: ${interiorService}</p>
+            </div>
         </div>`
 }
 
 const getAllAppts = () => axios.get('/api/appts')
     .then(({data}) => {
-        let dbObj = {data}.data
-        console.log(typeof(dbObj));
         allApptsDiv.innerHTML = "";
-
-        for (let apptObj in dbObj) {
-            let apptCard = makeAppointmentCard(apptObj)
+        data.forEach(obj => {
+            let apptCard = makeAppointmentCard(obj)
             allApptsDiv.innerHTML += apptCard
-        }
+        })
     })
     .catch(err => {console.log(err)
         alert("Error loading content")});
 
 getAllAppts()
+
+// let dbObj = [
+//     {
+//       "user_address_id": 2,
+//       "street_address": "10880 Malibu Point",
+//       "city": "Malibu",
+//       "state": "Utah",
+//       "user_id": 2,
+//       "appt_id": 1,
+//       "appt_date": "2022-07-26",
+//       "interior": true,
+//       "appt_price": "100"
+//     },
+//     {
+//       "user_address_id": 2,
+//       "street_address": "10880 Malibu Point",
+//       "city": "Malibu",
+//       "state": "Utah",
+//       "user_id": 2,
+//       "appt_id": 2,
+//       "appt_date": "2022-10-01",
+//       "interior": true,
+//       "appt_price": "100"
+//     }
+//   ]
+
+// const getAllAppts = (arr) => {
+//     allApptsDiv.innerHTML = "";
+//     arr.forEach(obj => {
+//         let apptCard = makeAppointmentCard(obj)
+//         allApptsDiv.innerHTML += apptCard
+//     })
+// }
+
+//   getAllAppts(dbObj);
